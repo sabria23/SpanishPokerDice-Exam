6ab716fdbbba6ded21227a5f59c781806ecfcd7d
sabrina <sabrina130102@gmail.com>
2026-05-06 12:48:28 +0200

    Add footer component with styling

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,15 +1,15 @@
-import styles from "./Footer.module.css";
+import Styles from "./Footer.module.css";
 
 export default function Footer() {
     return(
         <footer>
-            <div className={styles["container"]}>
-                <div className={styles["links"]}>
+            <div className={Styles["container"]}>
+                <div className={Styles["links"]}>
                     <a href="/aboutUs">About us</a>
                     <a href="/privacy">Privacy policy</a>
                     <a href="/terms">Terms  Conidtions</a>
                 </div>
-            <p className={styles.footer}>© Spanish Poker Dice 2026</p>
+            <p className={Styles.footer}>© Spanish Poker Dice 2026</p>
             </div>
             
         </footer>
