200a3ee44faf641d8f81e56030e8df342abb7d68
sabrina <sabrina130102@gmail.com>
2026-05-06 12:47:44 +0200

    Add navbar with responsive styling

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,27 @@
-import { NavLink } from "react-router";
-import styles from "./Navbar.module.css";
+import { NavLink, Link } from "react-router";
+import Styles from "./Navbar.module.css";
 
 export default function NavBar() {
+  const itemCss = Styles["nav-element"];
+
+  function styleIt({ isActive }) {
+    return isActive
+      ? `${itemCss} ${Styles.active}`
+      : itemCss;
+  }
+
 
   return (
-    <nav className={styles.nav}>     
-      <div className={styles.links}>
-        <NavLink  to="/">Home</NavLink>
+    <nav className={Styles.nav}>   
+      <Link to="/" className={Styles.icon}>
+        Spanish-Poker-Dice
+      </Link>  
+      
+      <div className={Styles.links}>
+        <NavLink className={styleIt}  to="/tournaments">Tournaments</NavLink>
+        <NavLink className={styleIt}  to="/lobby">Lobby</NavLink>
+        <NavLink className={styleIt}  to="/howToPlay">How to play</NavLink>
+        <NavLink className={styleIt}  to="/about">About</NavLink>
       </div>
     </nav>
   );
