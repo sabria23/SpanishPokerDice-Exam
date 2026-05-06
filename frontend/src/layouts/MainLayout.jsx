1fa867f94ea3421745b705e95e02964f990bd53a
sabrina <sabrina130102@gmail.com>
2026-05-06 12:50:12 +0200

    Improve global layout and base styling

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -4,12 +4,12 @@ import Footer from "@/components/Footer/Footer";
 
 export default function MainLayout(){
 	return (
-		<>
+		<div className="layout">
 			<Header/>
-			<main>
+			<main className="content">
 				<Outlet/>
 			</main>
 			<Footer/>
-		</>
+		</div>
 	);
 }
